323e273383b8c6364b1f2206dcc3bcfdb79c9bc9
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2024-01-14 21:33:14 +0900

    Feat : Modify mosaic code
    - train_mosaic 폴더에 mosaic 이미지 저장
    - train_mosaic.json에 mosaic 이미지의 기본 정보와 bbox 정보 추가해서 저장
    - #19

--- Upsampling/mosaic.py
+++ Upsampling/mosaic.py
@@ -117,8 +117,15 @@ def Mosaic(img_folder, indices, annotations):
         # Labels
         labels = []
         for anno in annotations[i]:
-            labels.append([anno["category_id"], *anno["bbox"]])  # xywh
+            top_left_x, top_left_y, box_w, box_h = anno["bbox"]
+            center_x = top_left_x + box_w / 2
+            center_y = top_left_y + box_h / 2
+            labels.append(
+                [anno["category_id"], center_x / s, center_y / s, box_w / s, box_h / s]
+            )  # xywh
         labels = np.array(labels)
+        # print(index)
+        # print(labels)
 
         if labels.size:
             labels[:, 1:] = xywh2xyxy(
@@ -126,9 +133,12 @@ def Mosaic(img_folder, indices, annotations):
             )  # normalized xywh to pixel xyxy format
             labels[labels < 0] = 0
             labels[labels > s * 2] = s * 2
-        labels4.append(labels)
+
+        if len(labels) > 0:
+            labels4.append(labels)
 
     # Concat/clip labels
+    # print(labels4)
     labels4 = np.concatenate(labels4, 0)
 
     # no padding
@@ -140,46 +150,138 @@ def Mosaic(img_folder, indices, annotations):
     labels4[:, 1:] = xyxy2xywh(labels4[:, 1:], w=xmax - xmin, h=ymax - ymin)
     labels4 = labels4[labels4[:, 3] > 0]
     labels4 = labels4[labels4[:, 4] > 0]
+    labels4[:, 1:] = xywh2xyxy(labels4[:, 1:], w=img4.shape[1], h=img4.shape[0])
 
     return img4, labels4
+
+
+def label2annotation(image_id, bbox_id, labels):
+    annotations = []
+    for i in range(len(labels)):
+        category_id = int(labels[i][0])
+        xmin = round(labels[i][1], 1)
+        ymin = round(labels[i][2], 1)
+        xmax = round(labels[i][3], 1)
+        ymax = round(labels[i][4], 1)
+        w = xmax - xmin
+        h = ymax - ymin
+        bbox = [xmin, ymin, w, h]
+        annotations.append(
+            {
+                "image_id": image_id,
+                "category_id": category_id,
+                "area": w * h,
+                "bbox": bbox,
+                "iscrowd": 0,
+                "id": bbox_id + i,
+            }
+        )
+    return annotations
 
 
 if __name__ == "__main__":
     # image files and labels
     # train set & not Scratch
-    data_path = "/data/ephemeral/home/level2-objectdetection-cv-08/data/recycle"
-    img_folder = os.path.join(data_path, "train")
-    im_files = [i for i in os.listdir(img_folder) if i.endswith("jpg")]
+    mode = "save"
 
-    # random 4 images
-    indices = random.choices(
-        im_files, k=4
-    )  # 3 additional image indices # [0, 3292, 20762, 18713]
-    random.shuffle(indices)  # [18713, 0, 20762, 3292]
+    # save : upsampling 해서 저장하는 모드
+    if mode == "save":
+        data_path = "/data/ephemeral/home/level2-objectdetection-cv-08/data/recycle"
+        img_folder = os.path.join(data_path, "train")
+        img_files = [i for i in os.listdir(img_folder) if i.endswith("jpg")]
 
-    # train.json 파일 경로
-    json_path = os.path.join(data_path, "train.json")
+        # train.json 파일 경로
+        json_path = os.path.join(data_path, "train_eye_eda.json")
 
-    # JSON 파일에서 이미지 및 바운딩 박스 정보 읽기
-    with open(json_path, "r") as json_file:
-        data = json.load(json_file)
+        # JSON 파일에서 이미지 및 바운딩 박스 정보 읽기
+        with open(json_path, "r") as json_file:
+            data = json.load(json_file)
 
-    annotations = []
-    for index in indices:
-        annotations.append(
-            [anno for anno in data["annotations"] if anno["image_id"] == int(index[:4])]
+        cnt = 4000
+        image_id = 100000
+        bbox_id = 100000
+        for i in range(cnt):
+            # random 4 images
+            indices = random.choices(
+                img_files, k=4
+            )  # 3 additional image indices # [0, 3292, 20762, 18713]
+            random.shuffle(indices)  # [18713, 0, 20762, 3292]
+
+            # indices에 맞는 annotation 뽑기
+            annotations = []
+            for index in indices:
+                annotations.append(
+                    [
+                        anno
+                        for anno in data["annotations"]
+                        if anno["image_id"] == int(index[:4])
+                    ]
+                )
+
+            # 이미지랑 label 추출 (label은 xyxy 상태)
+            img4, labels4 = Mosaic(img_folder, indices, annotations)
+
+            # json의 images에 추가할 image 정보
+            image_info = {
+                "width": 1024,
+                "height": 1024,
+                "file_name": os.path.join(
+                    "train_mosaic", "{0:06d}.jpg".format(image_id)
+                ),
+                "id": image_id,
+            }
+
+            # json의 annotations에 추가할 bbox 정보
+            bbox_info = label2annotation(image_id, bbox_id, labels4)
+
+            # 이미지 저장
+            img = Image.fromarray(img4)
+            img.save(
+                os.path.join(data_path, "train_mosaic", "{0:06d}.jpg".format(image_id)),
+                "JPEG",
+            )
+            # 기존 json에 추가
+            data["images"].append(image_info)
+            data["annotations"].extend(bbox_info)
+
+            image_id += 1
+            bbox_id = bbox_info[-1]["id"] + 1
+
+        # mosaic 파일 추가된 json 파일 생성
+        new_json_path = os.path.join(data_path, "train_mosaic.json")
+        with open(new_json_path, "w") as json_file:
+            json.dump(data, json_file, indent=4)
+
+        print(f"Data saved to {new_json_path}")
+
+    # test : mosaic 이미지랑 bbox 잘 나오는지 확인만 하는 모드
+    if mode == "Test":
+        # random 4 images
+        indices = random.choices(
+            img_files, k=4
+        )  # 3 additional image indices # [0, 3292, 20762, 18713]
+        random.shuffle(indices)  # [18713, 0, 20762, 3292]
+
+        annotations = []
+        for index in indices:
+            annotations.append(
+                [
+                    anno
+                    for anno in data["annotations"]
+                    if anno["image_id"] == int(index[:4])
+                ]
+            )
+
+        img4, labels4 = Mosaic(img_folder, indices, annotations)
+
+        img = Image.fromarray(img4)
+        img.save(
+            "/data/ephemeral/home/level2-objectdetection-cv-08/Upsampling/mosaic_sample.jpg",
+            "JPEG",
         )
 
-    img4, labels4 = Mosaic(img_folder, indices, annotations)
-
-    img = Image.fromarray(img4)
-    img.save(
-        "/data/ephemeral/home/level2-objectdetection-cv-08/Upsampling/mosaic_sample.jpg",
-        "JPEG",
-    )
-
-    img_bbox = myFig(img4, labels4)
-    img_bbox.save(
-        "/data/ephemeral/home/level2-objectdetection-cv-08/Upsampling/mosaic_sample_bbox.jpg",
-        "JPEG",
-    )
+        img_bbox = myFig(img4, labels4)
+        img_bbox.save(
+            "/data/ephemeral/home/level2-objectdetection-cv-08/Upsampling/mosaic_sample_bbox.jpg",
+            "JPEG",
+        )
